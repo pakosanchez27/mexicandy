48648b81c6214d6d029f10643b854877280b3a05
Francisco Javier Sanchez <javier.sanchezjps27@gmail.com>
2023-04-03 06:22:19 -0600

    Creando proyecto

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('.menu');
+
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
+  menu.classList.toggle('active');
+});
